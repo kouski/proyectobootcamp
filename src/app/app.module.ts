da45f32e76193d438c544ad520a5841fdf5562b5
kouski <72550619+kouski@users.noreply.github.com>
2023-12-29 22:22:03 +0100

    Componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatChipsModule} from '@angular/material/chips';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [	
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
       
       
    ],
