c8d1534bd2819978bdc819eb1cc262a0dee4fb7f
kouski <72550619+kouski@users.noreply.github.com>
2023-12-29 20:51:52 +0100

    creacion footer y navbar

---
+++ .angular/cache/17.0.7/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,20 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-ZAXN3KS3.js";
+import "./chunk-XQ2W6HDO.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-TMF34ROS.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
